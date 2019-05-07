ebcdc15a8fe3118b2659a4484ef600762b292a48
Aresun <2014229503@qq.com>
2019-05-07 20:43:25 +0800

    mobile not support template string; concat it

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
 const testDom = document.getElementById("test");
-testDom.textContent = `client width: ${document.documentElement.clientWidth}`;
+testDom.textContent = "client width: " + document.documentElement.clientWidth;
