59488f8db55e3fe96763a42c4b13f63afbb58bf6
Aresun <2014229503@qq.com>
2019-05-07 21:16:20 +0800

    change initial scale

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
 const testDom = document.getElementById("test");
+const dpr = window.devicePixelRatio;
+
 let test = "client width: " + document.documentElement.clientWidth;
-test += "dpr: " + window.devicePixelRatio;
+test += "dpr: " + dpr;
 testDom.textContent = test;
+
+// scale 1/dpr
+document
+  .getElementByName("viewport")[0]
+  .setAttribute("content", "width=device-width,initail-scale=" + 1 / dpr);
